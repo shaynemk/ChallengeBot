7b639c6320f4486b73e3b721892f47f7f8ec29c8
Shayne Keller <uskickbutt@gmail.com>
2020-09-01 15:11:52 -0700

    converted to 'bot' vs 'client'

--- bot.py
+++ bot.py
@@ -6,36 +6,45 @@ from discord.ext import commands
 
 # load variables
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CMD_PREFIX = os.getenv('CMD_PREFIX')
+#GUILD = os.getenv('DISCORD_GUILD')
 
+#client = discord.Client()
 
-client = discord.Client()
+#@client.event
+#async def on_error(event, *args, **kwargs):
+#    with open('err.log', 'a') as f:
+#        if event == 'on_message':
+#            f.write(f'Unhandled message: {args[0]}\n')
+#       else:
+#            raise
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+#@client.event
+#async def on_ready():
+#    guild = discord.utils.get(client.guilds, name=GUILD)
+#
+#    print(
+#        f'{client.user} is connected to the following guild:\n'
+#        f'{guild.name} (id: {guild.id})'
+#    )
 
-@client.event
+#@client.event
+#async def on_message(msg):
+#    if msg.author == client.user:
+#        return
+#    
+#    if msg.content == "!@#raise-exception":
+#        raise discord.DiscordException
+#    
+#    #other stuff
+
+#client.run(TOKEN)
+
+bot = commands.Bot(command_prefix=CMD_PREFIX)
+
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{bot.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name} (id: {guild.id})'
-    )
-
-@client.event
-async def on_message(msg):
-    if msg.author == client.user:
-        return
-    
-    #other stuff
-
-
-client.run(TOKEN)
+bot.run(DISCORD_TOKEN)
