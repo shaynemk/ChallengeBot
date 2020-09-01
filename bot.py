c03e9fe7d42022d1644c78343ba91fc6ce72f8f1
Shayne Keller <uskickbutt@gmail.com>
2020-09-01 15:04:15 -0700

    some tweaking, added error logging

--- bot.py
+++ bot.py
@@ -1,24 +1,41 @@
 # bot.py
 import os
-
-import discord
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 
+# load variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+CMD_PREFIX = os.getenv('CMD_PREFIX')
+
 
 client = discord.Client()
 
 @client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+@client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
-        f'{client.user} is connected to the following guilds:\n'
+        f'{client.user} is connected to the following guild:\n'
         f'{guild.name} (id: {guild.id})'
     )
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    
+    #other stuff
+
 
 client.run(TOKEN)
