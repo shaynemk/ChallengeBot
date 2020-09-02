a6fd718dc4118ec855757a03b747b7a827673539
Shayne Keller <uskickbutt@gmail.com>
2020-09-01 18:24:00 -0700

    tweaks and bobs

--- bot.py
+++ bot.py
@@ -1,50 +1,126 @@
 # bot.py
-import os
+import os, json, discord, datetime
 from dotenv import load_dotenv
-import discord
 from discord.ext import commands
 
-# load variables
+# load variables from '.env' file
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CMD_PREFIX = os.getenv('CMD_PREFIX')
-#GUILD = os.getenv('DISCORD_GUILD')
+CH_CHALLENGE = os.getenv('CH_CHALLENGE')
+CH_GENERAL = os.getenv('CH_GENERAL')
+RO_CHALLENGED = os.getenv('RO_CHALLENGED')
+RO_CHALLENGER = os.getenv('RO_CHALLENGER')
 
-#client = discord.Client()
+# init other variables
+HELP_ADMIN_NEW = 'cbadmin new <name> <startdate> <enddate>'
+HELP_CB = 'Use this command to document your challenge progress (requires Challenged role)'
+JSON_CHALLENGES_FILE = 'challenges.json'
+JSON_TEMPLATE = 'template.json'
+CHALLENGES = {}
+TEMPLATE = {}
+newChallenge = {}
 
-#@client.event
-#async def on_error(event, *args, **kwargs):
-#    with open('err.log', 'a') as f:
-#        if event == 'on_message':
-#            f.write(f'Unhandled message: {args[0]}\n')
-#       else:
-#            raise
 
-#@client.event
-#async def on_ready():
-#    guild = discord.utils.get(client.guilds, name=GUILD)
-#
-#    print(
-#        f'{client.user} is connected to the following guild:\n'
-#        f'{guild.name} (id: {guild.id})'
-#    )
+#---------------------------------------------
+# begin helpers
+#---------------------------------------------
+def readJSON():
+    with open(JSON_CHALLENGES_FILE) as f:
+        CHALLENGES = json.load(f)
+    with open(JSON_TEMPLATE) as f:
+        TEMPLATE = json.load(f)
 
-#@client.event
-#async def on_message(msg):
-#    if msg.author == client.user:
-#        return
-#    
-#    if msg.content == "!@#raise-exception":
-#        raise discord.DiscordException
-#    
-#    #other stuff
+def writeJSON():
+    with open(JSON_CHALLENGES_FILE, 'w') as f:
+        json.dump(CHALLENGES, f)
 
-#client.run(TOKEN)
+def init_JSON():
+    readJSON()
+    print(json.dumps(CHALLENGES, indent=2))
 
+def startNewChallenge():
+    newChallenge = TEMPLATE.copy()
+
+def createNewChallenge():
+    newChallengeTime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    CHALLENGES[newChallengeTime] = newChallenge
+
+
+#---------------------------------------------
+# begin bot specifics
+#---------------------------------------------
 bot = commands.Bot(command_prefix=CMD_PREFIX)
 
 @bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='cbadmin new', help=HELP_ADMIN_NEW)
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_new(ctx, _name, _startDate, _endDate):
+    if ctx.message.author == bot.user:
+        return
+    startNewChallenge()
+    newChallenge['name'] = _name
+    newChallenge['start'] = _startDate
+    newChallenge['end'] = _endDate
+
+@bot.command(name='cbadmin start', help='start date')
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_start(ctx, _startDate):
+    if ctx.message.author == bot.user:
+        return
+    newChallenge['start'] = _startDate
+
+@bot.command(name='cbadmin end', help='end date')
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_end(ctx, _endDate):
+    if ctx.message.author == bot.user:
+        return
+    newChallenge['end'] = _endDate
+
+@bot.command(name='cbadmin req add', help='cbadmin req add <key> <number>')
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_req_add(ctx, _key, _value : int):
+    if ctx.message.author == bot.user:
+        return
+    newChallenge['requirements'][_key] = _value
+
+@bot.command(name='cbadmin req del', help='cbadmin req del <key> <number>')
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_req_del(ctx, _key, _value : int):
+    if ctx.message.author == bot.user:
+        return
+    newChallenge['requirements'][_key].pop
+
+@bot.command(name='cbadmin create', help='#asdf')
+@commands.has_role(RO_CHALLENGER)
+async def cbadmin_create(ctx):
+    if ctx.message.author == bot.user:
+        return
+    createNewChallenge()
+
+
+@bot.command(name='cb')
+@commands.has_role(RO_CHALLENGED)
+async def cb(ctx):
+    if ctx.message.author == bot.user:
+        return
+
+
+init_JSON()
 bot.run(DISCORD_TOKEN)
